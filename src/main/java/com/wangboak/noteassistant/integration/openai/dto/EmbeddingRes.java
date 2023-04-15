0f01292b7ae7729b1bec4ecbfbb3d615b16b97e6
wangbo23 <wangbo23@kuaishou.com>
2023-04-15 19:59:05 +0800

    add git ignore

--- src/main/java/com/wangboak/noteassistant/integration/openai/dto/EmbeddingRes.java
+++ src/main/java/com/wangboak/noteassistant/integration/openai/dto/EmbeddingRes.java
@@ -2,6 +2,8 @@ package com.wangboak.noteassistant.integration.openai.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 /**
@@ -18,8 +20,12 @@ public class EmbeddingRes {
     Usage usage;
 
     @Data
-    private static final class Usage {
+    public static final class Usage {
+
+        @JsonProperty(value = "prompt_tokens")
         private Integer promptTokens;
+
+        @JsonProperty(value = "total_tokens")
         private Integer totalTokens;
     }
 
