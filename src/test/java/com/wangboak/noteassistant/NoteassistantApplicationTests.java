31a193dbd50d40491ee2add19f5e2de9ee63307e
wangbo23 <wangbo23@kuaishou.com>
2023-04-15 15:09:23 +0800

    first commit

---
+++ src/test/java/com/wangboak/noteassistant/NoteassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wangboak.noteassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
